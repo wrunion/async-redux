c82a6d50548ccb5f9199b165ad3b7ba9d9be1dba
wrunion <winterrunion@gmail.com>
2020-05-10 09:42:03 -0700

    Add async action using thunk.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+/* TO DISPATCH A FUNCTION W/THUNK: MANUALLY CALL DISPATCH!! */
+export const fetchPosts = () => {
+  /* Make the API call using async await */
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get('./posts');
+    /* This returns an *object*, which is okay since we're using thunk, but we need to manuually call dispatch here. */
+    dispatch({ type: 'FETCH_POSTS', payload: response});  
+  }
+};
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>Post List Here</div>
@@ -8,4 +14,11 @@ class PostList extends Component {
   }
 }
 
-export default PostList;
+// const mapStateToProps = (state) => {
+//   return { posts: state.postList }
+// }
+
+export default connect(
+  null, 
+  { fetchPosts }
+)(PostList);
